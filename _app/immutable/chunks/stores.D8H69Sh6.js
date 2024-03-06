a05980a2c3de3b0978b6e372c10ff2e5cca02fa7
JPratama7 <josepratama080@gmail.com>
2024-03-06 14:10:39 +0700

    Updates

---
+++ _app/immutable/chunks/stores.D8H69Sh6.js
@@ -0,0 +1,1 @@
+import{s}from"./entry.BmgbivPp.js";const e={subscribe:e=>(()=>{const e=s;return{page:{subscribe:e.page.subscribe},navigating:{subscribe:e.navigating.subscribe},updated:e.updated}})().page.subscribe(e)};export{e as p};
--- _app/immutable/entry/start.CiEqi4IM.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/entry.DPw486G_.js";export{r as start};

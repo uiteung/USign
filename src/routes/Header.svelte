4a32e6384a54adb7974ac6f34722229f2a8f25e3
JPratama7 <josepratama080@gmail.com>
2024-03-06 15:05:27 +0700

    fix: header url di button error

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
@@ -19,13 +20,13 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="{base}/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="{base}/about">About</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/pdf' ? 'page' : undefined}>
-				<a href="/pdf">PDF</a>
+				<a href="{base}/pdf">PDF</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">

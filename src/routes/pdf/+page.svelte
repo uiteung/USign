690f154359ce827bc60e5df1d41f279cb747bfe1
JPratama7 <josepratama080@gmail.com>
2024-03-06 14:07:37 +0700

    feat: editor pdf

--- src/routes/pdf/+page.svelte
+++ src/routes/pdf/+page.svelte
@@ -1,45 +1,44 @@
 <script lang="ts">
-	import { FileHandler } from '$lib/file';
+	// import { FileHandler } from '$lib/file';
 
-	import Pdf from '$components/Pdf.svelte';
+	// import Pdf from '$components/Pdf.svelte';
 
-	import Editor from '$components/PDF/editor/App.svelte';
+	import Editor from '$components/PDF/Editor.svelte';
 
-	import Swal from 'sweetalert2';
+	// import Swal from 'sweetalert2';
 
-	import { Modal } from 'flowbite-svelte';
+	// import { Modal } from 'flowbite-svelte';
 
 
-	let showPdf: boolean = false;
+	// let showPdf: boolean = false;
 
-	let files: FileList;
-	let encodedPdf: Uint8Array;
+	// let files: FileList;
+	// let encodedPdf: Uint8Array;
 
-	const uploadFile = (event: Event) => {
-		event.preventDefault();
-		FileHandler(files[0])
-			.then((file) => {
-				file.match({
-					some: (file) => {
-						console.log(file);
-						encodedPdf = new Uint8Array(file);
-					},
-					none: () => {
-						throw new Error('File not found');
-					}
-				});
-			})
-			.then(() => {
-				showPdf = true;
-			})
-			.catch((error) => {
-				Swal.fire({
-					title: 'Error',
-					text: error.message,
-					icon: 'error'
-				});
-			});
-	};
+	// const uploadFile = (event: Event) => {
+	// 	event.preventDefault();
+	// 	FileHandler(files[0])
+	// 		.then((file) => {
+	// 			file.match({
+	// 				some: (file) => {
+	// 					console.log(file);
+	// 					encodedPdf = new Uint8Array(file);
+	// 				},
+	// 				none: () => {
+	// 					throw new Error('File not found');
+	// 				}
+	// 			});
+	// 		})
+	// 		.then(() => {
+	// 			showPdf = true;
+	// 		})
+	// 		.catch((error) => {
+	// 			Swal.fire({
+	// 				title: 'Error',
+	// 				text: error.message,
+	// 				icon: 'error'
+	// 			});
+	// };
 </script>
 
 <!--<form on:submit={uploadFile}>-->

8df591d6b7f9cdf7add6d237e806a6ff51a60add
JPratama7 <josepratama080@gmail.com>
2024-03-06 21:04:02 +0700

    feat: login componen untuk auth

---
+++ src/components/Login.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import axios from 'axios';
+	import Swal from 'sweetalert2';
+	import {getCookie} from 'typescript-cookie';
+
+	if (getCookie("login")?.trim() === "") {
+		window.location.href = "/login";
+	}
+
+	axios.
+		post("https://auth.ulbi.ac.id/api/whatsauth/token")
+		.catch(() => {
+		Swal.fire({
+			title: "Login",
+			text: "Sepertinya Login kamu expire atau kamu belum login, silahkan login terlebih dahulu yaa.....",
+			icon: "question",
+			showConfirmButton: true,
+			showDenyButton: true
+		}).then((res) => {
+			if (res.isConfirmed) {
+				window.location.href = "/";
+			}
+		});
+		}
+	)
+
+</script>

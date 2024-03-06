a2658fb2da5e5ad7414460aea701b68844644470
JPratama7 <josepratama080@gmail.com>
2024-03-06 14:22:13 +0700

    feat: base env dari cicd

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			'$components': './src/components',
 		},
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/USign' : '',
+			base: process.env.BASE_PATH,
 		}
 	}
 };

be82f5e340f4a479246a89a6f06e69060c3f3c0c
JPratama7 <josepratama080@gmail.com>
2024-03-06 14:08:40 +0700

    feat: settings untuk build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
+		target: "es2022",
+		cssMinify: "lightningcss",
 		cssCodeSplit: true,
-		minify: 'terser'
+		minify: 'terser',
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'es2022'
+		}
 	}
 });
